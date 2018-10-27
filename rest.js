e6c9b6622bbeb251d03b84ace03bb59d56da5128
Levspir <levspir56@gmail.com.com>
2018-10-27 16:17:15 +0300

    added querry string parametrs

--- rest.js
+++ rest.js
@@ -1,6 +1,9 @@
-fetch('https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=3d63ab34a2954ad6b7d44a9ebaa218c1', {
+const url = new URL("https://api.nytimes.com/svc/books/v3/lists/overview.json"),
+    params = {"api-key": "3d63ab34a2954ad6b7d44a9ebaa218c1", "published_date": "2013-05-22"};
+Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+fetch(url, {
     method: 'GET',
-    })
+})
     .then((request) => request.json())
     .then((data)=> data.results["lists"])
     .then((data)=> {
@@ -9,4 +12,6 @@ fetch('https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=3d63ab34
             console.log(i["books"].length)
         }
     });
+
+
 
